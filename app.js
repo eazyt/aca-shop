5e52bb1780c82a21d485056a4db290f00dec46ae
eazyt <ramohlalen@gmail.com>
2020-10-30 19:04:04 +0200

    styling pages - 5-20

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const shopRoutes = require('./routes/shop')
 PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use('/admin', adminRoutes)
