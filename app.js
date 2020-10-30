3a1755cac9a6b2b544facffcbc01d94b32bea6dc
eazyt <ramohlalen@gmail.com>
2020-10-30 21:20:03 +0200

    ejs - 6

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const adminRoutes = require('./routes/admin')
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 PORT = process.env.PORT || 3000
@@ -13,14 +16,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => { 
   console.log('Not Found Middleware')
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
   // res.sendFile(path.join(__dirname, 'views', '404.html'))
-    // res.sendFile(path.join(__dirname, 'views', '404.html'))
-  res.sendFile(path.join(__dirname, 'views', '404.html'))
   // res.send('Not FOUNDS')
   // next()
 })
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,23 @@
 const path = require('path')
 const express = require('express')
 const rootDir = require('../util/path')
+const adminData = require('./admin')
 const router = express.Router()
 
 
 router.get('/', (req, res, next) => { 
   console.log('in the root page')
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  // console.log('shod.js',adminData.products)
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  const products = adminData.products
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'shop',
+    path: '/',
+    hasProducts: adminData.products.length > 0,
+    activeShop: true,
+    productsCSS: true
+  })
 })
 
 
